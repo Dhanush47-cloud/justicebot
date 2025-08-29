9894ad170ad6073d9a51692d40d2ec7b18993926
Dhanush Bangera <dhanushbangera390@gmail.com>
2025-08-29 23:14:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ app.post('/webhook', async (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+//app.listen(3000, () => console.log('Server running on port 3000'));
